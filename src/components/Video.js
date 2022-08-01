80b2d828adb15350a98c1d4328647d5f49739245
Omar Alsakka <djomrofficial@gmail.com>
2022-08-01 20:38:34 +0300

    fixed the background loop and title

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,8 +2,8 @@ import earthVideo from "../media/Earth1.mp4";
 
 const Video = () => {
   return (
-    <video className="myVideo" muted autoPlay playsInline>
-      <source loop="true" src={earthVideo} type="video/mp4" />
+    <video className="myVideo" muted autoPlay playsInline loop>
+      <source src={earthVideo} type="video/mp4" />
     </video>
   );
 };
